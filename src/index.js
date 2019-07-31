9cf3eecc7259037a5653f515d4800fbd3733eb9f
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-07-31 11:59:48 -0400

    bomb

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Drill1 from './state-drills';
+// import Drill1 from './state-drills/HelloWorld';
+import Bomb from './state-drills/Bomb';
 
-ReactDOM.render(<Drill1 />, document.getElementById('root'));
+ReactDOM.render(<Bomb />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
